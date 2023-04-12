b666c88788a81740bef2229eb5f8a432545faff0
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2023-04-12 13:27:21 +0100

    Audio added to the code

--- java/src/ie/tudublin/Flocking.java
+++ java/src/ie/tudublin/Flocking.java
@@ -1,9 +1,42 @@
 package ie.tudublin;
 import processing.core.PApplet;
 import processing.core.PImage;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
 
 public class Flocking extends PApplet
 {
+  
+  //Audio related things
+  Minim minim;
+  AudioPlayer ap;
+  AudioInput ai;
+  AudioBuffer ab;
+
+  int mode = 0;
+
+  float u = 0;
+  float smoothedY = 0;
+  float smoothedAmplitude = 0;
+  
+  //Pauses the audio with spacebar if needed
+  public void keyPressed() {
+  if (key >= '0' && key <= '9') {
+    mode = key - '0';
+  }
+  if (keyCode == ' ') {
+          if (ap.isPlaying()) {
+              ap.pause();
+          } else {
+              ap.rewind();
+              ap.play();
+          }
+      }
+}
+  
+  // Variable for the image
   PImage camp;
   // The Boid class
  
@@ -14,8 +47,14 @@ public class Flocking extends PApplet
   }
  public void setup() 
  {
-  
+
+  minim = new Minim(this);
+  // Load the audio file
+  ap = minim.loadFile("chucky.mp3", 1024);
+        ap.play();
+        ab = ap.mix;
   background(0);
+  // Load the image
   camp = loadImage("forestcampfire.jpg");
    flock = new Flock();
    // Add an initial set of boids into the system
@@ -43,7 +82,7 @@ public class Flocking extends PApplet
         }
         for(int i = 0; i < 3; i++)
         {
-            flock.run();
+          flock.run();
         }
         
    
