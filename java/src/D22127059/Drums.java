17a4aaf3145855c5aceef6dfd9cf74a093c31de1
Kim McGrath <d22127059@mytudublin.ie>
2023-04-17 02:23:18 +0100

    drumkit attempt1

--- java/src/D22127059/Drums.java
+++ java/src/D22127059/Drums.java
@@ -1,111 +1,62 @@
 package D22127059;
 
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
-import processing.core.PApplet;
+import ie.tudublin.Visual;
+import ie.tudublin.VisualException;
 
-public class Drums extends PApplet
-{
-    Minim minim;
-    AudioPlayer ap;
-    AudioInput ai;
-    AudioBuffer ab;
+public class Drums extends Visual {
 
-    int mode = 0;
-
-    float y = 0;
-    float smoothedY = 0;
-    float smoothedAmplitude = 0;
-
-    public void settings()
-    {
-        size(1024, 1000, P3D);
-        //fullScreen(P3D, SPAN);
+    public void settings() {
+        size(800, 800, P3D);
     }
 
-    public void setup()
-    {
-        minim = new Minim(this);
-        ap = minim.loadFile("chucky.mp3", 1024);
-        ap.play();
-        ab = ap.mix;
+    public void setup() {
         colorMode(HSB);
-
-        y = height / 2;
-        smoothedY = y;        
+        setFrameSize(256);
+        startMinim();
+        loadAudio("chucky.mp3");
+        getAudioPlayer().play();
     }
 
-    float off = 0;
+    float radius = 200;
 
-    float lerpedBuffer[] = new float[1024];
-
-    public void draw()
-    {
-        float halfH = height / 2;
-        float average = 0;
-        float sum = 0;
-        off += 1;
-    
-    
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            sum += abs(ab.get(i));
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+    public void draw() {
+        calculateAverageAmplitude();
+        try {
+            calculateFFT();
+        } catch (VisualException e) {
+            e.printStackTrace();
         }
-        average= sum / (float) ab.size();
-    
-        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
-        
-        float cx = width / 2;
-        float cy = height / 2;
-    
+        calculateFrequencyBands();
         background(0);
-
-        stroke(255, 255);
-        strokeWeight(10);
-        float r = halfH * 0.5f;
-        float t = TWO_PI / ab.size();
-        beginShape();
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            float c = map(ab.get(i), -1, 1, 0, 255);
-            float amplitude = lerpedBuffer[i] * halfH * 1.0f;
-            float x = cx + cos(t * i) * (r - amplitude); // subtract amplitude from radius
-            float y = cy + sin(t * i) * (r - amplitude); // subtract amplitude from radius
-            vertex(x, y);
+        stroke(65);
+        camera(0, -500, 500, 0, 0, 0, 0, 1, 0);
+    
+        float[] bands = getSmoothedBands();
+        int numOfCylinders = 5;
+        float spacing = 50;
+        float startX = -spacing * (numOfCylinders / 2.0f) + (spacing / 1.0f);
+        int frontCylinderIndex = 2; // index of the cylinder in front of others
+            
+        for (int i = 0; i < numOfCylinders; i++) {
+            float x = startX + i * spacing;
+            float h = bands[i];
+            pushMatrix();
+            translate(x, -h / 2, 0);
+            
+            if (i == frontCylinderIndex) { // check if this cylinder is in front
+                rotateX(PI/2); // rotate cylinder to face the camera
+            }
+    
+            beginShape(QUAD_STRIP);
+            for (int j = 0; j <= 360; j += 20) {
+                float vx = cos(radians(j)) * 25;
+                float vz = sin(radians(j)) * 25;
+                vertex(vx, -h / 2, vz);
+                vertex(vx, h / 2, vz);
+            }
+            endShape();
+            popMatrix();
         }
-
-        endShape(CLOSE); 
-
-        stroke(74);
-        strokeWeight(15);
-        float r2 = halfH * 0.5f;
-        beginShape();
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            float c = map(ab.get(i), -1, 1, 0, 255);
-            float amplitude = lerpedBuffer[i] * halfH * .0f;
-            float x = cx + cos(t * i) * (r2 - amplitude); // subtract amplitude from radius
-            float y = cy + sin(t * i) * (r2 - amplitude); // subtract amplitude from radius
-            vertex(x, y);
-        }
-        endShape(CLOSE);  
-          
-        stroke(444);
-        strokeWeight(15);
-        float r3 = halfH * 0.5f;
-        beginShape();
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            float c = map(ab.get(i), -1, 1, 0, 255);
-            float amplitude = lerpedBuffer[i] * halfH * .0f;
-            float x = cx + cos(t * i) * (r3 - amplitude); // subtract amplitude from radius
-            float y = cy + sin(t * i) * (r3 - amplitude); // subtract amplitude from radius
-            vertex(x, y);
-        }
-        endShape(CLOSE); 
-
-    }       
+    }
+    
 }
