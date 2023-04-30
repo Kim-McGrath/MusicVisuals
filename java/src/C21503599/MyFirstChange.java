c8a9e4fb128215b2cbea1e8f52d19e417f0bffcc
Lee <echomartin12@gmail.com>
2023-04-30 23:14:05 +0100

    Commented and added colour change on beat

--- java/src/C21503599/MyFirstChange.java
+++ java/src/C21503599/MyFirstChange.java
@@ -16,6 +16,8 @@ public class MyFirstChange extends Visual {
     BeatDetect beat;
     AudioBuffer ab;
     AudioPlayer ai;
+    int redN = 220;
+    int gb = 50;
 
     public void settings() {
         size(1024, 768, P3D);
@@ -30,6 +32,8 @@ public class MyFirstChange extends Visual {
         loadAudio("chucky.mp3"); 
         getAudioPlayer().play();
         lights();
+        frameRate(100);
+        frameRate(50);
     }
     
 //(500 + abs(sin(frameCount*(float)0.01)) * 500)
@@ -38,28 +42,32 @@ public class MyFirstChange extends Visual {
         beat.detect(getAudioPlayer().mix);
         blendMode(NORMAL);
         background(0);
-
+        
+        //Skews the perspective of the view making stuff appear a little wider
         perspective(PI/3, width/height, 10, 1000000);
 
         beginCamera();
+        //centers visual
         translate(width/2, height/2, 0);
+
+        //spin sphere
         rotateY((float)0.04);
         rotateX((float)0.01);
 
 
         stroke(255);
         calculateAverageAmplitude();
-        
-        // for (int i = 0; i < getAudioBuffer().size(); i++) {
-        //     stroke(255, getAudioBuffer().get(i)*500);
-        //     line(i * (float)(width/getAudioBuffer().size()), (float)(height/2) + getAudioBuffer().get(i)*300, i * (float)(width/getAudioBuffer().size()), (float)(height/2) - getAudioBuffer().get(i)*300);
-        // }
 
+        //stopped audio issues
         hint(DISABLE_DEPTH_TEST);
+        //make stroke appear more 3D
         hint(ENABLE_STROKE_PERSPECTIVE);
         int total = 100;
+        
+        //2d arr to store the coords of sphere
         PVector[][] PVa = new PVector[total][total];
 
+        //calculate co-ordinates
         for (int i = 0; i < total; i++) {
             float lat = map(i, 0, total - 1, -HALF_PI, HALF_PI);
             for (int j = 0; j < total; j++) {
@@ -74,11 +82,15 @@ public class MyFirstChange extends Visual {
             }
         }
 
+        //overlapping colour adds towards white
         blendMode(ADD);
 
+        //Creates sphere from co-ordinates using triangles
+        //Changes alpha based on amplitude each frame with a with a cap on 255
+        //The red, green and blue get changed by the beat detect to switch to a light blue
         for (int i = 0; i < total - 1; i++) {
             beginShape(TRIANGLE_STRIP);
-            stroke(222, 50, 50, getAudioBuffer().get(i)*500);
+            stroke(redN, gb, gb,  min(getAudioBuffer().get(i)*500, 255));
             noFill();
             for (int j = 0; j < total -1; j++) {
                 vertex(PVa[i][j].x, PVa[i][j].y, PVa[i][j].z);
@@ -86,7 +98,16 @@ public class MyFirstChange extends Visual {
             }
             endShape();
         }
-        if (beat.isOnset()) background(255);
+        redN = 220;
+        gb = 50;
+
+        //flicker backround and change sphere colours on beat
+        if (beat.isOnset()) {
+            background(255);
+            redN = 0;
+            gb = 255;
+        } 
+        //cancels out translation so it doesn't go flying off screen
         translate(-(width/2), -(height/2), 0);
         endCamera();
     }   
