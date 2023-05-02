ffd5cb8343f651f6d31a56f1dc4da3d2c2a0dd0c
Kim McGrath <d22127059@mytudublin.ie>
2023-05-02 20:58:13 +0100

    added kim to main

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,23 +1,12 @@
 package ie.tudublin;
 
-// Lee's Visuals
-import C21503599.MyFirstChange;
+import D22127059.Circle; //Kim Visual
 
-import D22127059.Circle;
-// Example's that can be run
-import example.RotatingAudioBands;
 
 
 
 public class Main
 {	
-
-	public void startUI()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyFirstChange());		
-	}
-
 
 	public static void circle()
 	{
@@ -25,11 +14,6 @@ public class Main
         processing.core.PApplet.runSketch( a, new Circle());
     }
 
-	public static void RotatingAudioBands()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());
-    }
 
 	public static void main(String[] args)
 	{
