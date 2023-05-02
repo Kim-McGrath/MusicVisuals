d097f1cb949e82deb0df7f16450ea0ca885f335c
Kim McGrath <d22127059@mytudublin.ie>
2023-05-02 21:02:03 +0100

    k

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,13 +1,7 @@
 package ie.tudublin;
 
-<<<<<<< HEAD
 import D22127059.Circle; //Kim Visual
 
-=======
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
->>>>>>> fe4543543e6076ea7af0d032723d353338540781
 
 public class Main
 {	
@@ -15,14 +9,9 @@ public class Main
 	public static void circle()
 	{
 		String[] a = {"MAIN"};
-<<<<<<< HEAD
         processing.core.PApplet.runSketch( a, new Circle());
     }
 
-=======
-        processing.core.PApplet.runSketch( a, new MyVisual());		
-	}
->>>>>>> fe4543543e6076ea7af0d032723d353338540781
 
 	public static void main(String[] args)
 	{
