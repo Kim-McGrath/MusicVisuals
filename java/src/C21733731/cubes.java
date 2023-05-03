a412434b7e17d8e2763e1b981a6466f6234fbd47
Kim McGrath <d22127059@mytudublin.ie>
2023-05-03 15:52:36 +0100

    rw

--- java/src/C21733731/cubes.java
+++ java/src/C21733731/cubes.java
@@ -4,10 +4,10 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class cubes {
-    PApplet p;
-    PVector position;
-    PVector velocity;
-    float size;
+    PApplet p; //stores a reference to the main applet
+    PVector position; //store the position of the cubes
+    PVector velocity; //store the velocity of the cubes
+    float size; //size of the cubes
 
     public cubes(PApplet p, PVector position, float size){
         this.p = p;
@@ -18,24 +18,21 @@ public class cubes {
 
     }
     public void render(){
-        p.pushMatrix();
+        p.pushMatrix(); // Store the current transformation matrix
         p.translate(position.x, position.y, position.z);
-        p.box(size);
-        p.popMatrix();
+        p.box(size); // Draw a box with the specified size
+        p.popMatrix(); // Restore the previous transformation matrix
     }
 
     public void update(){
-        position.add(velocity);
+        position.add(velocity); // Add the velocity vector to the position vector to update the position of the cubes
     }
 
     public void antigravity(PVector gravPoint, float min, float max){
         PVector temp;
 
         temp = PVector.sub(position, gravPoint);
-        //temp.x = PApplet.constrain(temp.x, min, max);
-        //temp.y = PApplet.constrain(temp.y, min, max);
-        //temp.z = PApplet.constrain(temp.z, min, max);
-        temp.mult(0.06f);
+        temp.mult(0.06f); //speed
         velocity.set(temp);
 
 
